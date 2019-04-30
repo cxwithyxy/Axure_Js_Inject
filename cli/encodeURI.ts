d626ef5ddaae08e2ae2da01d37219a08cc0a5fdb
chenxuan <523764720@qq.com>
2019-04-30 17:24:43 +0800

    encodeURI 了

--- cli/encodeURI.ts
+++ cli/encodeURI.ts
@@ -1,4 +1,12 @@
 import fs from "fs";
 
-console.log(process.argv);
+let file_path = process.argv[2];
 
+let file_content = fs.readFileSync(file_path, {encoding: `utf-8`})
+
+file_content = file_content.replace(new RegExp("\r", "g"), "神奇的换行符")
+file_content = file_content.replace(new RegExp("\n", "g"), "神奇的回车符")
+
+file_content = encodeURIComponent(file_content);
+
+fs.writeFileSync(file_path, file_content, {encoding: `utf-8`})
